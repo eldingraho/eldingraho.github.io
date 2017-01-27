dc6801bcbe625d412a4e248637a217f31db9d996
Eldin <eldingraho@gmail.com>
2017-01-27 13:29:56 +0100

    fix main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
     if('serviceWorker' in navigator){
         window.addEventListener('load', function(){
-            navigator.serviceWorker.register('sw/sw.js').then(function(registration){
+            navigator.serviceWorker.register('/sw/sw.js').then(function(registration){
                 console.log('success: serviceWorker registered at - '+registration.scope)
             }).catch(function(err){
                 console.log('failde: serviceWorker registration failed err: ', err);
