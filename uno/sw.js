a19428034bd9875e27f7efac1a26bd7d25b083aa
Eldin <eldingraho@gmail.com>
2017-01-27 14:11:52 +0100

    add main.js in index of uno

--- uno/sw.js
+++ uno/sw.js
@@ -8,6 +8,18 @@ self.addEventListener('install', function (event) {
         caches.open(CACHE_NAME)
             .then(function (cache) {
                 return cache.addAll(urls)
-            })
+            }
+        )
+    )
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (respond) {
+                if (respond)
+                    return respond;
+                return fetch(event.request)
+            }
+        )
     )
 });
